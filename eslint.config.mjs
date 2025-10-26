772174fdf90489634541b6c56fdb1b79e0595067
Alesya Adamovich <131908857+aliadamovich@users.noreply.github.com>
2025-10-26 17:11:24 -0300

    Dev (#5)
    * init: add eslint rules, prettier, husky
    
    * fix husky errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,49 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    rules: {
+      // Formatting / code style
+      'max-len': ['error', { code: 120 }],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      ],
+
+      // React / JSX
+      'react/react-in-jsx-scope': 'off',
+      'react/function-component-definition': [
+        'error',
+        { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+      ],
+      'react/jsx-props-no-spreading': 'off',
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+
+      // Safety
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-magic-numbers': ['error', { ignore: [0, 1, -1, 200, 404, 500] }],
+      'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state', 'self'] }],
+
+      //  import order
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+          pathGroups: [{ pattern: 'react', group: 'external', position: 'before' }],
+          pathGroupsExcludedImportTypes: ['react'],
+        },
+      ],
+    },
   },
 ];
 
